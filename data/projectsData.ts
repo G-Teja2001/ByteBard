19ca4f76b7e5f27620dbfe512101d26d58592206
G-Teja2001 <helloteja2001@gmail.com>
2024-01-08 13:09:22 +0530

    first commit

---
+++ data/projectsData.ts
@@ -0,0 +1,18 @@
+const projectsData = [
+  // {
+  //   title: 'A Search Engine',
+  //   description: `What if you could look up any information in the world? Webpages, images, videos
+  //   and more. Google has many features to help you find exactly what you're looking
+  //   for.`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
+  {
+    title: 'Smart Bin Design',
+    description: 'Revolutionizing waste management with the Smart Bin - a solution that minimizes effort, maximizes efficiency, and eliminates unnecessary spillage.',
+    imgSrc: '/static/images/smartbin.jpg',
+    href: '/blog/smart-bin',
+  },
+]
+
+export default projectsData
